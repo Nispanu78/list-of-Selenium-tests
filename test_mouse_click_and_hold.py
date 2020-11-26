c80d49beabb1551353eb383504086f13fc05c85b
Nicola Spanu <nicspanu@gmail.com>
2020-11-26 16:00:26 +0100

    14

---
+++ test_mouse_click_and_hold.py
@@ -0,0 +1,22 @@
+import unittest
+from selenium import webdriver
+
+class MouseClickAndHold(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(30)
+
+        self.driver.get("http://www.apress.com")
+
+    def test_mouse_click_and_hold(self):
+        button = self.driver.find_element_by_link_text("Apress Access")
+        webdriver.ActionChains(driver=self.driver).click_and_hold(button).perform()
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
