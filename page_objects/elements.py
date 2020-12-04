64b2b373773fcd98e1d1f18e4a88ac593faab435
Nicola Spanu <nicspanu@gmail.com>
2020-12-04 13:02:35 +0100

    27

--- page_objects/elements.py
+++ page_objects/elements.py
@@ -8,6 +8,7 @@ class BasePageElement(object):
         WebDriverWait(driver, 100).until(lambda driver: driver.find_element_by_name(self.locator))
         driver.find_element_by_name(self.locator).clear()
         driver.find_element_by_name(self.locator).send_keys(value)
+
     def __get__(self, obj, owner):
 # Gets the text of the specified object
         driver = obj.driver
--- page_objects/test.py
+++ page_objects/test.py
@@ -1,20 +1,25 @@
 import unittest
 from selenium import webdriver
-import page
+from page_objects import page
 
 class ApressSearch(unittest.TestCase):
+# Sample test case using Page Object Model
     def setUp(self):
         self.driver = webdriver.Firefox()
+# Visits apress.com
         self.driver.get("https://www.apress.com")
 
     def test_apress_search(self):
         home_page = page.HomePage(self.driver)
+# Searches Python Selenium keyword
         home_page.search_text ="Python Selenium"
         home_page.click_submit_button()
         search_results_page = page.ResultPage(self.driver)
-        
+# Checks if page is not empty
         assert search_results_page.check_search_results(), "No results found."
+
     def tearDown(self):
         self.driver.close()
+
 if __name__ =="__main__":
     unittest.main()
